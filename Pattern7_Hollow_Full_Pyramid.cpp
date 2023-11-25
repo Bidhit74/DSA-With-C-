02d8bfffd2f3f9a4d3898c21fdff2fa37c6e8fe4
Bidhit <bidhitkumar61@gmail.com>
2023-11-26 01:50:32 +0530

    Hollow and Full Pyramid

---
+++ Pattern7_Hollow_Full_Pyramid.cpp
@@ -0,0 +1,67 @@
+// Full Pyramid
+// ROW = 6 AND COL = 11
+//           *
+//         * * *
+//       * * * * *
+//     * * * * * * *
+//   * * * * * * * * *
+// * * * * * * * * * * *
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 6;
+
+    for(int row = 0; row < n; row++){
+        for(int space = 0; space < n+1-row; space++){
+            cout << "  ";
+        }
+        for(int col = 0; col < (row*2)+1; col++){
+            cout << "* ";
+        }
+        cout<< endl;
+    }
+
+    return 0;
+}
+*/
+// Hollow Full Pyramid
+// ROW = 6 AND COL = 11
+//           *
+//         *   *
+//       *       *
+//     *           *
+//   *               *
+// * * * * * * * * * * *
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 20;
+
+    for(int row = 0; row < n; row++){
+        int col = 0;
+        for(int space = 0; space < (2*n)-1; space++){
+            if(space<n-row-1){
+                cout << "  ";
+            }
+            else{
+                if(col==0||col==2*row||row == n-1){
+                    cout << "* ";
+                }
+                else{
+                    cout << "  ";
+                }
+                col++;
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
