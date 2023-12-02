c577d3bbfb05758b9055facd763497163cfcf334
Bidhit <bidhitkumar61@gmail.com>
2023-12-02 07:07:34 +0530

    Basic Function

---
+++ DAY 4/Lecture 5 - Function/Functions.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void printName(){ // creating a function
+    // function body
+    for(int i = 0;i<5;i++){
+        cout<<"Bidhit"<<endl;
+    }
+}
+int main(){
+    printName(); // function call
+    return 0;
+}
