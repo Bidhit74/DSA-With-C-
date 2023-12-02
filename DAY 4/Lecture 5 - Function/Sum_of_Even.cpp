74ed7e2d8ecb4a27e9e09d426996cbd17a9ded0f
Bidhit <bidhitkumar61@gmail.com>
2023-12-02 08:20:42 +0530

    Sum of even number

---
+++ DAY 4/Lecture 5 - Function/Sum_of_Even.cpp
@@ -0,0 +1,24 @@
+// Sum of even number.Upto N
+
+#include <iostream>
+using namespace std;
+
+int sumOfEvenNumber(int n){
+    int sum = 0;
+    for(int i = 1; i <= n; i++){
+        if(i%2 == 0){
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of sum of even numbers: ";
+    cin >> n;
+    cout << "Sum of even number : " << sumOfEvenNumber(n) << endl;
+
+    return 0;
+}
