93fb0c43c97e146ed67b562839b6ce4fe2e79b18
Bidhit <bidhitkumar61@gmail.com>
2023-12-08 12:47:41 +0530

    Butterfly Pattern

---
+++ DAY 2/Lecture 3 - Patterns_Continued/Pattern18_Butterfly.cpp
@@ -0,0 +1,54 @@
+// Butterfly Pattern
+
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int r = 0; r < n; r++){
+        for(int c = 0; c < r+1; c++){
+            cout << "*";
+        }
+        for(int space = 1; space < n-r; space++){
+            cout << " ";
+        }
+        for(int space = 1; space < n-r; space++){
+            cout << " ";
+        }
+        for(int c = 0; c < r+1; c++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int r = 0; r < n; r++){
+        for(int c = 0; c < n-r; c++){
+            cout << "*";
+        }
+        for(int space = 1; space < r+1; space++){
+            cout << " ";
+        }
+        for(int space = 1; space < r+1; space++){
+            cout << " ";
+        }
+        for(int c = 0; c < n-r; c++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
