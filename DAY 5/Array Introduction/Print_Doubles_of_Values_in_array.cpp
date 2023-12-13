034d0fd7df66447426d94c9837420179dec3fdb5
Bidhit <bidhitkumar61@gmail.com>
2023-12-13 22:39:45 +0530

    multiply by 2 and print

---
+++ DAY 5/Array Introduction/Print_Doubles_of_Values_in_array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int arr[5] = {2,4,6,8,10};
+    for(int i = 0;i<5;i++){
+        // Just multiply by 2 and print
+        cout<<2*arr[i]<<" ";
+    }
+    return 0;
+}
