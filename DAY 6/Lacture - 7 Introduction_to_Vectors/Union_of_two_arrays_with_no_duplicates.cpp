4ebbe1e58a628fac1c0f227e5dcc03c5ebe136ff
Bidhit <bidhitkumar61@gmail.com>
2023-12-18 07:12:51 +0530

    Array

--- DAY 6/Lacture - 7 Introduction_to_Vectors/Union_of_two_arrays_with_no_duplicates.cpp
+++ DAY 6/Lacture - 7 Introduction_to_Vectors/Union_of_two_arrays_with_no_duplicates.cpp
@@ -1,26 +1,38 @@
 // Brute force approach is discussed -> We will discuss that if ans 
-// is returned in sorted array, there are duplicates as well in the array. 
-// In future we will discuss it.
+// is returned in sorted array, there are duplicates as well in the array.  ,,,,
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
-    int arr[] = {1,3,5,7,9};
-    int sizea = 5;
-    int brr[] = {2,4,6,8};
-    int sizeb = 4;
-    vector<int>ans;
+
+vector<int> unionOfArray(vector<int>arrA,vector<int>arrB){
+        vector<int>ans;
+        vector<int>answer;
     // putting array elements of a in ans array
-    for(int i=0;i<sizea;i++){
-        ans.push_back(arr[i]);
+    for(int i=0;i<arrA.size();i++){
+        ans.push_back(arrA[i]);
     }
-    // putting array elements of b in ans array
-    for(int i=0;i<sizeb;i++){
-        ans.push_back(brr[i]);
+
+    for(int i=0;i<arrB.size();i++){
+        bool found = false;
+        for(int j=0;j<ans.size();j++){
+            if(arrB[i]==ans[j]){
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            ans.push_back(arrB[i]);
+        }
     }
-    // printing ans array
-    for(int i=0;i<ans.size();i++){
-        cout<<ans[i]<<" ";
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {1,3,5,7,9,2};
+    vector<int> brr = {2,4,5,6,8,3,7};
+
+    for(auto value:unionOfArray(arr,brr)){
+        cout<< value<<" ";
     }
     return 0;
 }
