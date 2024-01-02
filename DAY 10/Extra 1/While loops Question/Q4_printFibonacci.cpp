a0249e982e68f9df1f5ee66f499576a259a3bbf0
Bidhit <bidhitkumar61@gmail.com>
2024-01-02 10:01:13 +0530

    Print the nth fibonacci number

---
+++ DAY 10/Extra 1/While loops Question/Q4_printFibonacci.cpp
@@ -0,0 +1,23 @@
+// Question - Print the nth fibonacci number.
+// n = 6;
+// output = series = 1 1 2 3 5 8
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Number of fibonacci :";
+    cin >> n;
+    int first = 1 , second = 1 , sum = 0;
+
+    for(int i = 1; i <= n; i++){
+        cout << first << " ";
+        sum = first + second;
+        first = second;
+        second = sum;
+    }
+
+    return 0;
+}
