eebb038d83d5addc9a2b76fbb945629f7312dedb
Bidhit <bidhitkumar61@gmail.com>
2024-02-16 07:09:10 +0530

    Sort the array of 0's and 1's.

---
+++ DAY 6/Lacture - 7 Introduction_to_Vectors/Question/sortArray_0_and_1.cpp
@@ -0,0 +1,50 @@
+// Question - Sort the array of 0's and 1's.
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// void mySort(vector<int>&v){
+//     int numZero = 0, numOne = 0;
+//     for(int i = 0; i < v.size();i++){
+//         if(v[i] == 0) 
+//             numZero++;
+//         else 
+//             numOne++;
+//     }
+//     for(int i = 0; i < v.size();i++){
+//         if(i<numZero) 
+//             v[i] = 0;
+//         else
+//             v[i] = 1;
+//     }
+// }
+
+// Two pointer methods
+
+void mySort(vector<int>&v){
+    int start = 0 , end = v.size()-1;
+    while(start < end){
+        if(v[start] == 0) start++;
+        if(v[end] == 1) end--;
+        if(v[start] == 1 && v[end] == 0){
+            int temp = v[start];
+            v[start] = v[end];
+            v[end] = temp;
+        }
+    }
+}
+
+int main(){
+    vector<int> v{1, 1, 0, 1, 0, 0, 1, 0, 1, 0};
+
+    for(auto v1:v){
+        cout << v1 << " ";
+    }
+    cout << endl;
+
+    mySort(v);
+    for(auto v1:v){
+        cout << v1 << " ";
+    }
+    return 0;
+}
