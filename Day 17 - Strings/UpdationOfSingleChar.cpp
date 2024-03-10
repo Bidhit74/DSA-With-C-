d2611785211d4f44124a136c28dcfb575ce47f78
Bidhit <bidhitkumar61@gmail.com>
2024-03-10 22:11:10 +0530

    Question Add

--- Day 17 - Strings/UpdationOfSingleChar.cpp
+++ Day 17 - Strings/UpdationOfSingleChar.cpp
@@ -1,12 +1,36 @@
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main()
+// {
+//     string str = "Bidhit";
+//     cout << str << endl;
+//     str[3] = 'i'; // In C++, string are mutable/chengable
+//     cout << str << endl;
+//     return 0;
+// }
+
+// Question : Input a string of size n and upadate all the even positions in the string to character 'a'. consider 0 - based indexing
+
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main()
 {
-    string str = "Bidhit";
+    int n;
+    cout << "How many lenght characters: ";
+    cin >> n;
+    cout << "Enter the characters: ";
+    char str[n];
+    for(int i = 0; i < n; i++){
+        cin>>str[i];
+    }
     cout << str << endl;
-    str[3] = 'i'; // In C++, string are mutable/chengable
+    for (int i = 0; i < n; i+=2){
+        str[i] = 'a';
+    }
     cout << str << endl;
     return 0;
 }
