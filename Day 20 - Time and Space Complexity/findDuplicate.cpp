f71c0e1c8ae9bcd7e626036eb6150d0a7d4930b1
Bidhit <bidhitkumar61@gmail.com>
2024-03-15 15:34:15 +0530

    Method - 3 : (Using Math) Best Method Observation : Time and space is efficient

--- Day 20 - Time and Space Complexity/findDuplicate.cpp
+++ Day 20 - Time and Space Complexity/findDuplicate.cpp
@@ -30,23 +30,43 @@ using namespace std;
 // Observation : Time Efficient but space consuming
 // problem : We are using extra space
 
-int main()
-{
-    vector<int> arr = {3, 2, 1, 6, 2, 5};
-    int n = arr.size();
-    vector<int> v(n,0); // using extra space
-    bool flag = false;
-    for(int i = 0; i < n; i++){
-        v[arr[i]] += 1;
-        if(v[arr[i]]==2){
-            cout << arr[i] << endl;
-            flag = true;
-            break;
-        }
-    }
+// int main()
+// {
+//     vector<int> arr = {3, 2, 1, 6, 2, 5};
+//     int n = arr.size();
+//     vector<int> v(n,0); // using extra space
+//     bool flag = false;
+//     for(int i = 0; i < n; i++){
+//         v[arr[i]] += 1;
+//         if(v[arr[i]]==2){
+//             cout << arr[i] << endl;
+//             flag = true;
+//             break;
+//         }
+//     }
     
-    if(flag) cout << "Duplicate availabe" << endl;
-    else cout << "Duplicate not availabe" << endl;
+//     if(flag) cout << "Duplicate availabe" << endl;
+//     else cout << "Duplicate not availabe" << endl;
+    
+//     return 0;
+// }
+
+
+// Method - 3 : (Using Math) Best Method 
+// Observation : Time and space is efficient
+
+int main()
+{ 
+    vector<int> arr = {6,3,2,4,5,1,5};
+    int n = arr.size()-1;
+    int sum = 0;
+    for(int i = 0; i<=n; i++){
+        sum += arr[i];
+    }
+    int s = n *(n+1)/2;
+
+    cout << "Duplicate availabe: " <<(sum - s)<< " s: "<< s<< " sum : "<< sum<<endl;
+
     
     return 0;
 }
